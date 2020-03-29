79dbf6eb0d405371ade1b59797c48140c795b2e9
mantin2212 <noam.mantin@gmail.com>
2020-03-29 10:59:35 +0300

    Fixed fixtures scope.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from sudoku_solver.sudoku import Sudoku
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def solved_sudoku():
     """A fully solved sudoku board."""
     board = [[1, 2, 3, 4],
@@ -13,17 +13,18 @@ def solved_sudoku():
     return Sudoku(board, 2)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def empty_sudoku():
     """An empty sudoku board"""
     board = [["-"] * 4] * 4
     return Sudoku(board, 2)
 
 
-@pytest.fixture(scope="module")
-def illegal_sudoku(solved_sudoku):
-    """An illegal sudoku board"""
-    illegal_sudoku = solved_sudoku.clone()
-    # Two equal cells in the same row.
-    illegal_sudoku[0][0] = illegal_sudoku[0][1]
-    return illegal_sudoku
+@pytest.fixture()
+def illegal_sudoku():
+    """Illegal sudoku board."""
+    board = [[1, 1, 3, 4],
+             [3, 4, 1, 2],
+             [2, 3, 4, 1],
+             [4, 1, 2, 3]]
+    return Sudoku(board, 2)
